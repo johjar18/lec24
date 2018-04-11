3001f1db6a6c3817c205b66dfa4d44a68874079a
Jaren Johnson <johjar@umich.edu>
2018-04-10 23:49:07 -0400

    Finished

---
+++ bball.py
@@ -0,0 +1,28 @@
+def init_bball(csv_file_name=BB_FILE_NAME):
+    global bb_seasons
+    with open(csv_file_name) as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader) # throw away headers
+        global bb_seasons
+        bb_seasons = [] # reset, start clean
+        for r in reader:
+            bb_seasons.append(r)
+
+def get_bball_seasons(sortby='year', sortorder='desc'):
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return render_template("hello.html")
